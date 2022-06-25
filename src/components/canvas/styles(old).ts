80f2c23cd46698248b3417d47c481b40621b9691
anjaliahlawat16 <anjali@primesellerhub.com>
2022-06-25 17:32:05 +0200

    feat: changes in product requirements[#5]

---
+++ src/components/canvas/styles(old).ts
@@ -0,0 +1,19 @@
+import colors from "src/assets/colors";
+
+const styles = {
+  canvasWrapper: {
+    position: "relative",
+  },
+  canvas: {
+    minHeight: 400,
+    margin: 30,
+    backgroundColor: colors.white,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: colors.borderColor,
+    position: "absolute",
+  },
+};
+
+export default styles;
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,28 +1,7 @@
-import React, { ReactElement, useState } from "react";
+import React from "react";
 
-import ButtonGroupComponent from "../../components/buttongroup/ButtonGroupComponent";
-import CanvasComponent from "../../components/canvas/CanvasComponent";
-import ColumnComponent from "../../components/column/ColumnComponent";
-import styles from "./styles";
-
-function HomeScreen(): ReactElement {
-  const [selectionTool, setSelectionTool] = useState<string>();
-
-  return (
-    <div className="container-fluid homeScreen" style={styles.homeScreen}>
-      <div className="row">
-        <ColumnComponent col={3}>
-          <ButtonGroupComponent
-            selectionTool={selectionTool}
-            setSelectionTool={(tool) => setSelectionTool(tool)}
-          />
-        </ColumnComponent>
-        <ColumnComponent col={9}>
-          <CanvasComponent selectionTool={selectionTool} />
-        </ColumnComponent>
-      </div>
-    </div>
-  );
+function HomeScreen(props) {
+  return <div></div>;
 }
 
 export default HomeScreen;
